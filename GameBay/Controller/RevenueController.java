aebe9cdbcb753945ebeaf889f64d992754bcfafa
EmaR975 <72090610+EmaR975@users.noreply.github.com>
2021-04-16 17:57:42 -0400

    Create RevenueController.java

---
+++ GameBay/Controller/RevenueController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Seller;
+import View.RevenueView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ *
+ * @author Emily Rojas
+ */
+public class RevenueController {
+    
+    public RevenueController(RevenueView view, Seller model){
+        this.view = view;
+        this.model = model;
+        
+        double cost, revenue;
+        cost=this.model.getCost();
+        revenue=this.model.getRevenue();
+        
+        this.view.setCost(cost);
+        this.view.setRevenue(revenue);
+        this.model.findProfit();
+        this.view.setProfit(model.getProfit());
+        
+        this.view.homeActionListener(new HomeListener());
+    }
+    
+    class HomeListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            SellerController.main();
+        }
+        
+    }
+            
+    
+    private RevenueView view;
+    private Seller model;
+}
