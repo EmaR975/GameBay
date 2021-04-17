d616c169af0b23d75dcbe1ca79925489febf73cb
TruongNguyen4138 <82682031+TruongNguyen4138@users.noreply.github.com>
2021-04-17 16:50:23 -0400

    Add files via upload

---
+++ model/CartModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cart.model;
+
+/**
+ *
+ * @author truon
+ */
+public class CartModel {
+    
+    public void updateCart(){
+        
+    }
+    public void addItem(){
+        
+    }
+    public void removeItem(){
+        
+    }
+}
