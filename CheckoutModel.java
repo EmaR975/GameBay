d492e830771540d1637b7eca12bb28773e5705bc
TruongNguyen4138 <82682031+TruongNguyen4138@users.noreply.github.com>
2021-04-18 14:49:54 -0400

    Add files via upload

--- CheckoutModel.java
+++ CheckoutModel.java
@@ -9,5 +9,47 @@
  * @author truon
  */
 public class CheckoutModel {
-    //private String ;
+    private String cardName;
+    private String address;
+    private String phoneNum;
+    private String email;
+    
+    public void saveBillingInfo(String cardName, String address, String phoneNum, String email){
+        this.cardName = cardName;
+        this.address = address;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+    
+    public String getCardName(){
+        return cardName;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public String getPhoneNum(){
+        return phoneNum;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setCardName(String cardName){
+        this.cardName = cardName;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public void setPhoneNum(String phoneNum){
+        this.phoneNum = phoneNum;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
--- CheckoutView.java
+++ CheckoutView.java
@@ -12,89 +12,46 @@ import javax.swing.*;
  */
 public class CheckoutView extends JFrame{
     private JLabel title = new JLabel("CHECK OUT", SwingConstants.CENTER);
-    private JButton addressButton = new JButton("Add your shipping address");
-    private JButton billingButton = new JButton("Payment method");
-    private JButton reviewButton = new JButton("Review items");
-    private JButton orderButton = new JButton("Place your order");
-    
-    private JLabel aLabel = new JLabel("Please enter your shipping address information");
-    private JLabel nameLabel = new JLabel("Full name ");
-    private JTextField aName = new JTextField();
-    private JLabel addressLabel = new JLabel("Address ");
-    private JTextField address = new JTextField();
-    private JButton submitButton = new JButton("Submit");
-    
-    private JLabel bLabel = new JLabel("Add a credit or debit card");
-    private JLabel cardNumLabel = new JLabel("Card Number ");
-    private JTextField cardNumber = new JTextField();
-    private JLabel cardNameLabel = new JLabel("Name on card ");
+    private JLabel cardLabel = new JLabel("Name on the card");
     private JTextField cardName = new JTextField();
-    private JLabel cardExLabel = new JLabel("Expiration date ");
-    private JTextField cardExp = new JTextField();
-    private JLabel cardSecLabel = new JLabel("Security code (CVV)");
-    private JTextField cardSec = new JTextField();
-    //private JButton submitButton = new JButton("Submit");
+    private JLabel addressLabel = new JLabel("Shipping address");
+    private JTextField address = new JTextField();
+    private JLabel phoneLabel = new JLabel("Phone number");
+    private JTextField phoneNum = new JTextField();
+    private JLabel emailLabel = new JLabel("Email address");
+    private JTextField email = new JTextField();
+    private JButton confirmButton = new JButton("Confirm");      
     
     
     public void showCheckoutView(){
         JPanel checkoutPanel = new JPanel();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	this.setSize(500, 300);
-        checkoutPanel.setLayout(new GridLayout(5, 1));
+        checkoutPanel.setLayout(new GridLayout(0, 1));
         title.setFont(new Font("Serif", Font.BOLD, 25));
         checkoutPanel.add(title);
-        checkoutPanel.add(addressButton);
-        checkoutPanel.add(billingButton);
-        checkoutPanel.add(reviewButton);
-        checkoutPanel.add(orderButton);
+        checkoutPanel.add(cardLabel);
+        checkoutPanel.add(cardName);
+        checkoutPanel.add(addressLabel);
+        checkoutPanel.add(address);
+        checkoutPanel.add(phoneLabel);
+        checkoutPanel.add(phoneNum);
+        checkoutPanel.add(emailLabel);
+        checkoutPanel.add(email);
+        checkoutPanel.add(confirmButton);
         this.add(checkoutPanel);
         this.setVisible(true);
-    }
-    public void showAddressView(){
-        
-        JPanel aPanel = new JPanel();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setSize(500, 300);
-        aPanel.setLayout(new GridLayout(0,1));  
-        aLabel.setFont(new Font("Serif", Font.BOLD, 25));
-        aPanel.add(aLabel);
-        aPanel.add(nameLabel);
-        aPanel.add(aName);
-        aPanel.add(addressLabel);
-        aPanel.add(address);
-        aPanel.add(submitButton);
-        this.add(aPanel);
-        
-        this.setVisible(true);        
-    }
+    }    
     
-    public void showBillingView(){
-        JPanel cPanel = new JPanel();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setSize(600, 300);
-        cPanel.setLayout(new GridLayout(0,1));
-        bLabel.setFont(new Font("Serif", Font.BOLD, 25));
-        cPanel.add(bLabel);
-        cPanel.add(cardNumLabel);
-        cPanel.add(cardNumber);
-        cPanel.add(cardNameLabel);
-        cPanel.add(cardName);
-        cPanel.add(cardExLabel);
-        cPanel.add(cardExp);
-        cPanel.add(cardSecLabel);
-        cPanel.add(cardSec);
-        cPanel.add(submitButton);
-        this.add(cPanel);
+    void addConfirmListener(ActionListener listenForConfirmButton){
+        confirmButton.addActionListener(listenForConfirmButton);	
+    }
         
-        this.setVisible(true);  
-    } 
-    
     
     public static void main(String[] args){
         CheckoutView cv = new CheckoutView();
         cv.showCheckoutView();        
-        //cv.showAddressView();
-        //cv.showBillingView();
+        
     }
     
 }
