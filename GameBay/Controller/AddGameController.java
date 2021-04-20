7e7715125a35ab7cce44ae457e9fe4153838e08b
EmaR975 <72090610+EmaR975@users.noreply.github.com>
2021-04-20 03:55:57 -0400

    Create AddGameController.java

---
+++ GameBay/Controller/AddGameController.java
@@ -0,0 +1,64 @@
+package Controller;
+
+import Main.Inventory;
+import View.AddGameView;
+import Controller.SellerController;
+import java.util.List;
+import Main.Product;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Emily Rojas
+ */
+public class AddGameController {
+    public AddGameController(AddGameView view, Inventory model){
+        this.view = view;
+        this.model = model;
+        
+        this.view.submitButton(new submitListener());
+        
+    }
+    
+    class submitListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int i, q;
+            double in,s;
+            String n;
+            try{
+                i = view.getID();
+                n = view.getName();
+                q = view.getQuantity();
+                in = view.getInvoice();
+                s = view.getSelling();
+                
+                model.addProduct(i,n,q,in,s);
+                
+                view.setVisible(false);
+                
+                SellerController.main(new String[] {"main"});
+            }
+            catch(NumberFormatException ex){
+                view.displayErrorMessage("Fields are empty. No changes made.");
+                
+                view.setVisible(false);
+                
+                SellerController.main(new String[] {"main"});
+            }
+            
+        }
+    }
+    
+    public static void main(String[] args){
+        AddGameView view = new AddGameView();
+        Inventory model = new Inventory();
+        AddGameController controller = new AddGameController(view, model);
+        
+        view.setVisible(true);
+    }
+    
+    private AddGameView view;
+    private Inventory model;
+}
