d805b156e9bfe3dfa6afced3f4e74e4261eb5ae9
EmaR975 <72090610+EmaR975@users.noreply.github.com>
2021-04-20 03:57:05 -0400

    Update RevenueController.java

--- GameBay/Controller/RevenueController.java
+++ GameBay/Controller/RevenueController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Model.Seller;
@@ -37,7 +32,7 @@ public class RevenueController {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            SellerController.main();
+            SellerController.main(new String[] {"main"});
         }
     }
         
