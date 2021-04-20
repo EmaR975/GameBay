c27af858d03f049fd3e915845871b75cfe7f3a7b
EmaR975 <72090610+EmaR975@users.noreply.github.com>
2021-04-20 03:54:56 -0400

    Update RevenueView.java

--- GameBay/View/RevenueView.java
+++ GameBay/View/RevenueView.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
 import java.awt.*;
@@ -22,6 +17,7 @@ public class RevenueView extends JFrame{
         
         compPanel.setLayout(new BorderLayout());
        
+        title.setFont(new Font("Serif", Font.BOLD, 25));
         titlePanel.add(title);
         titlePanel.add(homeButton);
         
