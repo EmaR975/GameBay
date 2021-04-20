9fea11e451b1c1c12710018b09c07a905ae5fd1e
EmaR975 <72090610+EmaR975@users.noreply.github.com>
2021-04-20 03:59:01 -0400

    Update Seller.java

--- GameBay/Model/Seller.java
+++ GameBay/Model/Seller.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
+
+import Main.Inventory;
+import Main.Product;
 
 /**
  *
@@ -11,6 +9,26 @@ package Model;
  */
 public class Seller{
     
+    //functions for seller
+    public Seller(Product product){
+        this.product = product;
+    }
+    
+    public Seller(){
+        costs = 30;
+        revenue = 70;
+    }
+    
+    public Product getProduct() {
+        return product;
+    }
+    
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    
+
+    //functions for revenue
     public void findProfit(){
         profit = revenue - costs;
     }
@@ -35,9 +53,12 @@ public class Seller{
         return revenue;
     }
     
-    private double costs=1;
-    private double revenue=3;
+    
+    private double costs;
+    private double revenue;
     private double profit;
+    private Product product=null;
     
 }
+
 
